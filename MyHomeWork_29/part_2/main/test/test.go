789676441b1e2fda1cac8f3538308ce17d490796
Artem Semenov <tsedro00@yandex.ru>
2022-12-28 18:00:02 +0300

    FixBagulina

--- MyHomeWork_29/part_2/main/test/test.go
+++ MyHomeWork_29/part_2/main/test/test.go
@@ -11,21 +11,21 @@ func Test() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT)
 	var i int
+	ch := make(chan int)
 	for {
 		select {
 		case <-sig:
-			fmt.Printf("Завершаю работу, квадрат последнего числа %d", <-Duble(i))
+			fmt.Printf("Завершаю работу, квадрат последнего числа %d", <-Duble(i, ch))
 			return
 		default:
 		}
 		fmt.Print("Введите число или завершите работу:")
 		fmt.Scan(&i)
-		fmt.Println(<-Duble(i))
+		fmt.Println(<-Duble(i, ch))
 	}
 }
 
-func Duble(i int) chan int {
-	ch := make(chan int)
+func Duble(i int, ch chan int) chan int {
 	go func() {
 		i *= i
 		ch <- i
