cc36373a2c17be3a731aff81f97d47195131bbe6
Artem Semenov <tsedro00@yandex.ru>
2023-01-05 13:05:26 +0300

    Fix:Homework_29

--- MyHomeWork_29/part_2/main/main.go
+++ MyHomeWork_29/part_2/main/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "v/lesson/MyHomework/MyHomework_29/part_2/main/test"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"v/lesson/MyHomework/MyHomework_29/part_2/main/test"
+)
 
 func main() {
-	test.Test()
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT)
+	var i int
+	ch := make(chan int)
+	for {
+		select {
+		case <-sig:
+			fmt.Printf("Завершаю работу, квадрат последнего числа %d", <-test.Duble(i, ch))
+			return
+		default:
+		}
+		test.Vvod(i, ch)
+	}
 }
--- MyHomeWork_29/part_2/main/test/test.go
+++ MyHomeWork_29/part_2/main/test/test.go
@@ -2,28 +2,7 @@ package test
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 )
-
-func Test() {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGINT)
-	var i int
-	ch := make(chan int)
-	for {
-		select {
-		case <-sig:
-			fmt.Printf("Завершаю работу, квадрат последнего числа %d", <-Duble(i, ch))
-			return
-		default:
-		}
-		fmt.Print("Введите число или завершите работу:")
-		fmt.Scan(&i)
-		fmt.Println(<-Duble(i, ch))
-	}
-}
 
 func Duble(i int, ch chan int) chan int {
 	go func() {
@@ -31,4 +10,10 @@ func Duble(i int, ch chan int) chan int {
 		ch <- i
 	}()
 	return ch
+}
+
+func Vvod(i int, ch chan int) {
+	fmt.Print("Введите число или завершите работу:")
+	fmt.Scan(&i)
+	fmt.Println(<-Duble(i, ch))
 }
